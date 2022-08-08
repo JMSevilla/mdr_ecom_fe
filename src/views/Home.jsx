3e1731cb92af94f98de8693ac6c69fb81b7d50f1
Bryan_Palad <98245520+BryanPalad@users.noreply.github.com>
2022-08-08 20:40:04 +0800

    MDR40-16 (New Task)

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,12 +8,14 @@ import HeroBanner from './FrontPage/herobanner/HeroBanner'
 const Home = () => {
     return (
         <>
+        <div className='section'>
             <ApplicationBar title={'Ecommerce'} />
             <HeroBanner/>
             <SystemContainer maxWidth={'lg'}>
                 <Services/>
             </SystemContainer>    
             <AppFooter/>
+        </div>
         </>
     )
 }
