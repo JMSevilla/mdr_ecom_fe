69ed1d377bfb52f1e3f4338a6b47b35fb5a50715
Bryan_Palad <98245520+BryanPalad@users.noreply.github.com>
2022-09-10 20:17:44 +0800

    MDR40-045 Login FE (Client Request)

--- src/views/FrontPage/testimonials/tesimonials_field.js
+++ src/views/FrontPage/testimonials/tesimonials_field.js
@@ -8,20 +8,19 @@ import { Navigation, Pagination, Autoplay } from "swiper";
 const testimonials_field = () =>{
     return(
         <>
-        <Box id="testimonials" className='sm:h-[80vh] flex flex-col justify-center bg-secondary'>
+        <Box id="testimonials" className='section md:h-[70vh] h-auto flex flex-col justify-center bg-secondary'>
                 <SystemContainer maxWidth={'lg'}>
-                        <Box className='flex flex-col items-center gap-5'>
-                        <img src={testimonialOutline} alt='testimonial outline text' className='w-65 hidden sm:block' />
-                            <SystemTypography text={"What other people says?"} variant={'h5'} />
-                            <SystemTypography variant={'subtitle'} text={"Here are some of our client's feedback..."} isgutter={true} style={{fontStyle: 'italic', marginBottom: '50px'}}/>
+                        <Box className='flex flex-col items-center text-center gap-0.5'>
+                        <img src={testimonialOutline} alt='about us outline text' className='hidden sm:block'/>
+                        <h5 className='text-3xl font-semibold sm:text-2xl'>What Other People Says?</h5>
+                        <SystemTypography variant={'subtitle'} text={"Here are some of our client's feedback..."} isgutter={true} style={{fontStyle: 'italic', marginBottom: '50px'}}/>
                         </Box>
                     <Box style={{display: "flex"}}>
                             <AppSwiper
                             testimonial={true}
                             swiperData={testimonialData}
-                            // style={{width:'80%', height: '90%', borderRadius: '20px'}}
                             modules={[Autoplay,Pagination,Navigation]}
-                            className='w-80 h-95 rounded sm:w-full'
+                            className='rounded w-full lg:w-[90%] lg:h-[95%]'
                             />
                            
                         </Box>
