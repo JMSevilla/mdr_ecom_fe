5a4b7bdbafff7418b677866e3bc84a97439e0b21
Bryan_Palad <98245520+BryanPalad@users.noreply.github.com>
2022-10-04 15:40:50 +0800

    MDR40-075 recommit to resolve conflict

--- src/core/Spiels/routerSpiels.js
+++ src/core/Spiels/routerSpiels.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import {appBORouter, appRouter} from '../../routes/router'
+import {appRouter} from '../../routes/router'
 class routerSpiels {
     router = [
         {
             router_id : 1,
             router_obj : {
-                home : appRouter.Homepage.path,
-                bo_dashboard: appBORouter.Home.path
+                home : appRouter.Homepage.path
             }
         }
     ]
