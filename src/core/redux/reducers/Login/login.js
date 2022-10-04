ad3d6a68f155b4726e50ed00ac0fc6b346356477
JM Sevilla <44269169+JMSevilla@users.noreply.github.com>
2022-10-04 14:27:18 +0800

    MDR40-80 [SIGNOUT] Administrator signing out

--- src/core/redux/reducers/Login/login.js
+++ src/core/redux/reducers/Login/login.js
@@ -1,8 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-import FormService from "../../../service/apiservice";
+import { apiCallBegan } from "../../actions/Actions";
+import Process from '../../../service/data_process'
 
 const initialState = {
   logon_message: "",
+  signout_message : ''
 };
 
 const loginSlice = createSlice({
@@ -10,18 +12,36 @@ const loginSlice = createSlice({
   initialState,
   reducers: {
     LOGIN_FULFILLED: (state, action) => {
-      console.log(action.payload);
       state.logon_message = action.payload;
     },
+    SIGNOUT_FULFILLED: (state, action) => {
+      state.signout_message = action.payload
+    }
   },
 });
 
 export default loginSlice.reducer;
 
-const { LOGIN_FULFILLED } = loginSlice.actions;
+const { LOGIN_FULFILLED, SIGNOUT_FULFILLED } = loginSlice.actions;
+
+export const SIGNOUT_PROCESS = (object) => (dispatch) => {
+  return dispatch(
+    apiCallBegan({
+      url : 'signout/',
+      method: 'POST',
+      data: Process.ADMIN_SIGNOUT(object),
+      onSuccess: SIGNOUT_FULFILLED.type
+    })
+  )
+}
 
 export const LOGIN_ONPROCESS = (object) => (dispatch) => {
-  FormService.CLIENT_CONFIG_checkLogin(object).then((res) => {
-    dispatch(LOGIN_FULFILLED(res.data));
-  });
+  return dispatch(
+    apiCallBegan({
+      url : 'applogin',
+      method : 'POST',
+      data : Process.CLIENT_user_login(object),
+      onSuccess: LOGIN_FULFILLED.type
+    })
+  )
 };
