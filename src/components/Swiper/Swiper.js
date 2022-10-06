27fdda1a17fcc6b4aec06f9154f21b3a8d7720c4
Bryan_Palad <98245520+BryanPalad@users.noreply.github.com>
2022-10-06 15:55:24 +0800

    MDR40-104 (Student Registration Spiels Polishing)

--- src/components/Swiper/Swiper.js
+++ src/components/Swiper/Swiper.js
@@ -16,10 +16,10 @@ const AppSwiper = (props) => {
               <Box className='gap-8 mt-30 mx-6 md:flex'>
             <img src={item.img} alt="hero_banner_images" className='rounded w-full' />
               <Box className='flex flex-col gap-6'>
-                <h1 className="text-base mt-4 font-body text-white italic text-justify lg:text-lg lg:mt-0"> {item.feedbackDisc} </h1>
+                <h1 className="text-base mt-4 font-body text-black italic text-justify lg:text-lg lg:mt-0"> {item.feedbackDisc} </h1>
                 <Box className='flex flex-col mb-5' >               
                 <h1 className='font-subtitle font-bold text-red-600' > {item.creator} </h1>
-                <h1 className='font-body text-white'> {item.profession} </h1>
+                <h1 className='font-body text-black'> {item.profession} </h1>
                 <Rating defaultValue={item.ratingValue} precision={item.ratingPrecision} readOnly/>
                 </Box>   
               </Box>
