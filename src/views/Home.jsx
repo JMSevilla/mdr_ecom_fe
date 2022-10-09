8ae381677348cd7d5b85963b672993db8d2d8466
Bryan_Palad <98245520+BryanPalad@users.noreply.github.com>
2022-10-09 14:10:03 +0800

    MDR40-097 (Frontpage Enhancement > Social Media Integration > Messenger float chat)

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,6 +8,10 @@ import ContactUs from './FrontPage/contactus/ContactUs'
 import Testimonials from './FrontPage/testimonials/Testimonials'
 import { GlobalContext } from '../core/context/GlobalContext'
 import { localstoragehelper } from '../core/utils/storage'
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+const pageId = process.env.REACT_APP_FB_PAGE_ID;
+const appId = process.env.REACT_APP_FB_APP_ID;
+
 const Home = () => {
     const contextValues = useContext(GlobalContext)
     const { tokenScanned, adminScanned } = contextValues
@@ -27,6 +31,7 @@ const Home = () => {
             <Testimonials />
             <ContactUs/>
             <AppFooter/>
+            <MessengerCustomerChat pageId={pageId} appId={appId}/>
         </div>
     )
 }
