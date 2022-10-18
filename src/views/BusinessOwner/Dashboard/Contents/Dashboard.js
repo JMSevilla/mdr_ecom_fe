9a3cb85b6da8531be62e23c5445cf093c6fc2ac2
Bryan_Palad <98245520+BryanPalad@users.noreply.github.com>
2022-10-18 13:13:51 +0800

    MDR40-100 (Business Owner Dashboard > 4x4 [ Rows & Columns ] Cards in dashboard) & User Mgmt table enhancement

--- src/views/BusinessOwner/Dashboard/Contents/Dashboard.js
+++ src/views/BusinessOwner/Dashboard/Contents/Dashboard.js
@@ -1,8 +1,66 @@
 import React from 'react'
-
+import { Box } from '@mui/material'
+import { CardContent } from "@mui/material";
+import {motion} from 'framer-motion';
+import { SystemGrid, ApplicationCard } from '../../../../components';
 const Dashboard = () => {
   return (
-    <div>Admin Dashboard</div>
+    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+    <Box className="flex flex-col gap-4">
+       <h1 className='text-black text-4xl font-main'>Dashboard</h1>
+       <SystemGrid 
+         rowSpacing={1}
+         columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+         xs={8}
+         sm={3}
+         md={3}
+         GridItems = {
+           [
+             {
+               childrenId: 1,
+               children : <ApplicationCard 
+                 children={
+                   <CardContent>
+                     <h3>Users</h3>
+                   </CardContent>
+                 }
+               />
+             },
+             {
+               childrenId: 2,
+               children : <ApplicationCard 
+                 children={
+                   <CardContent>
+                     <h3>Users</h3>
+                   </CardContent>
+                 }
+               />
+             },
+             {
+               childrenId: 3,
+               children : <ApplicationCard 
+                 children={
+                   <CardContent>
+                     <h3>Users</h3>
+                   </CardContent>
+                 }
+               />
+             },
+             {
+               childrenId: 4,
+               children : <ApplicationCard 
+                 children={
+                   <CardContent>
+                     <h3>Users</h3>
+                   </CardContent>
+                 }
+               />
+             }
+           ]
+         }
+       />
+       </Box>
+ </motion.div>
   )
 }
 
